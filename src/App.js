5ee45f1bef7cc2f361bd274a63d6deb774d7eb94
Luis Rodriguez <luchoweb@Luiss-MacBook-Air.local>
2020-05-20 18:24:37 -0500

    comments for understand and remember

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/scss/bootstrap.scss';
 import './App.css';
 
 class App extends Component {
+  // create a new state!
   state = {
     persons: [
       { id: '1', name: 'Luis Augusto', age: 31 },
@@ -15,8 +16,8 @@ class App extends Component {
     showPersons: false
   }
 
+  // Change all persons in the state with new names!
   switchNameHandler = () => {
-    // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Marlie';
     this.setState({
       persons: [
@@ -27,13 +28,15 @@ class App extends Component {
     })
   }
 
-togglePersonsHandler = () => {
+  // Show or hide all persons with a click event
+  togglePersonsHandler = () => {
     this.setState({
       showPersons: !this.state.showPersons
     });
   }
 
 
+  // delete some person in the state with click event
   deletePersonHandler = (index) => {
     const persons = this.state.persons;
     persons.splice(index, 1);
@@ -43,15 +46,42 @@ togglePersonsHandler = () => {
     })
   }
 
-  render() {
-    const style = {}
+  // Change a person's name by input value
+  changeNameHandler = ( event ) => {
+    // Find index for this input event
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id === id;
+    });
 
+    // save the person with this index in a const
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // Change the name for this person
+    person.name = event.target.value;
+
+    // save the persons state in a const
+    const persons = [ ...this.state.persons ];
+    // change attrs values for person with index
+    persons[personIndex] = person;
+
+    // finally, save the new person to state
+    this.setState({ persons: persons });
+  }
+
+  render() {
+
+    // create a persons
     let persons = null;
 
+    // if show persons state is true!
     if ( this.state.showPersons ) {
+      // add JSX to persons
       persons = (
         <div>
           {
+            // mapping persons on state
             this.state.persons.map((person, index) => {
               return <Person
                 name={ person.name }
@@ -68,29 +98,20 @@ togglePersonsHandler = () => {
           </button>
         </div>
       );
-
-      style.backgroundColor = 'red';
-    }
-
-    const classes = [];
-    if ( this.state.persons.length <= 2 ) {
-      classes.push('red');
-    }
-    if ( this.state.persons.length <= 1 ) {
-      classes.push('bold')
     }
 
     return (
       <div className={`container App`}>
         <h1>It works!</h1>
-        <p className={ classes.join(' ') }>This is really working!</p>
+        <p>This is really working!</p>
+        <!-- Show / Hide Persons -->
         <button
-          style={ style }
           className={`btn btn-info`}
           onClick={ this.togglePersonsHandler }>
             { this.state.showPersons ? 'Hide' : 'Show' } persons
         </button>
         <hr />
+        <!-- Show persons find in the state-->
         { persons }
       </div>
     );
